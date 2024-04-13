273dabc133d15ae9d8fc354e86ed64462fcf553e
Jose Felipe Araujo <j.felipe.araujo@outlook.com>
2024-04-12 23:44:03 -0300

    test: :white_check_mark: add bdd tests

--- go.mod
+++ go.mod
@@ -1,9 +1,20 @@
-module github.com/jsfelipearaujo/fast-food-lambda-authorizer
+module github.com/jsfelipearaujo/lambda-authorizer
 
 go 1.21.3
 
 require (
 	github.com/aws/aws-lambda-go v1.46.0
+	github.com/cucumber/godog v0.14.0
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/google/uuid v1.6.0
+)
+
+require (
+	github.com/cucumber/gherkin/go/v26 v26.2.0 // indirect
+	github.com/cucumber/messages/go/v21 v21.0.1 // indirect
+	github.com/gofrs/uuid v4.3.1+incompatible // indirect
+	github.com/hashicorp/go-immutable-radix v1.3.1 // indirect
+	github.com/hashicorp/go-memdb v1.3.4 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 )
--- main.go
+++ main.go
@@ -1,68 +1,12 @@
 package main
 
 import (
-	"context"
 	_ "embed"
-	"errors"
-	"os"
 
-	"log/slog"
-
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/jsfelipearaujo/fast-food-lambda-authorizer/src/policy"
-	"github.com/jsfelipearaujo/fast-food-lambda-authorizer/src/token"
+	"github.com/jsfelipearaujo/lambda-authorizer/src/handler"
 )
-
-func init() {
-	opts := &slog.HandlerOptions{
-		Level: slog.LevelInfo,
-	}
-
-	handler := slog.NewTextHandler(os.Stdout, opts)
-	log := slog.New(handler)
-	slog.SetDefault(log)
-}
-
-var (
-	ErrUnauthorized = errors.New("Unauthorized")
-)
-
-func getTokenFromRequestHeaders(request events.APIGatewayCustomAuthorizerRequestTypeRequest) string {
-	headers := []string{"authorization", "Authorization"}
-
-	for _, header := range headers {
-		tokenString := request.Headers[header]
-		if tokenString != "" {
-			return tokenString
-		}
-	}
-
-	return ""
-}
-
-func handleRequest(ctx context.Context, request events.APIGatewayCustomAuthorizerRequestTypeRequest) (events.APIGatewayCustomAuthorizerResponse, error) {
-	tokenString := getTokenFromRequestHeaders(request)
-
-	if tokenString == "" {
-		slog.Error("no token found in request")
-		return events.APIGatewayCustomAuthorizerResponse{}, ErrUnauthorized
-	}
-
-	isValid, err := token.Validator(tokenString)
-	if err != nil {
-		slog.Error("error validating token: %v", err)
-		return events.APIGatewayCustomAuthorizerResponse{}, ErrUnauthorized
-	}
-
-	if isValid {
-		return policy.GenerateAllowPolicy(), nil
-	}
-
-	slog.Error("token is not valid")
-	return events.APIGatewayCustomAuthorizerResponse{}, ErrUnauthorized
-}
 
 func main() {
-	lambda.Start(handleRequest)
+	lambda.Start(handler.HandleRequest)
 }
